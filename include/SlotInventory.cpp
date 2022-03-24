b6a8988471c68f32685023087b5a39f6a142db45
Muhammad Gilang Ramadhan <80266785+gilanglahat22@users.noreply.github.com>
2022-03-24 22:24:24 +0700

    benerin slotInventory

--- include/SlotInventory.cpp
+++ include/SlotInventory.cpp
@@ -30,8 +30,20 @@ int SlotInventory::getQuantity() {
   return this->quantity;
 }
 
+int SlotInventory::getEmptyQuantity(){
+    return (64 - quantity);
+}
+
 Item* SlotInventory::getItem() {
   return this->slotcontainer;
+}
+
+string SlotInventory::getNameFromSlotItem(){
+    return this->slotcontainer[0].getName();
+}
+
+string SlotInventory::getTypeFromSlotItem(){
+    return this->slotcontainer[0].getType();
 }
 
 void SlotInventory::setEmptySlot() {
@@ -46,4 +58,22 @@ void SlotInventory::addQuantity(int quantity) {
 
 void SlotInventory::reduceQuantity(int quantity) {
   this->quantity = this->quantity - quantity;
+}
+
+void SlotInventory::addItemToSlot(Item* item, int Quantity){
+    int CurrentQuantity = Quantity + this->quantity;
+    for (int i = this->quantity; i < CurrentQuantity; i++){
+        this->slotcontainer[i] = item[i-this->quantity];
+    }
+    this->quantity = CurrentQuantity;
+}
+
+void SlotInventory::removeItem(int QuantityToRemove){
+    int CurrentQuantity = this->quantity - QuantityToRemove;
+    for (int i = CurrentQuantity; i < this->quantity; i++){
+        this->slotcontainer[i].setId(-1);
+        this->slotcontainer[i].setName("Unknown");
+        this->slotcontainer[i].setType("Unknown");
+    }
+    this->quantity = CurrentQuantity;
 }
