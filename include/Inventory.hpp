10814325e02f16caae4b7798c181087f14c60cc7
Muhammad Gilang Ramadhan <80266785+gilanglahat22@users.noreply.github.com>
2022-03-24 22:25:11 +0700

    Update Inventory.hpp

--- include/Inventory.hpp
+++ include/Inventory.hpp
@@ -2,10 +2,13 @@
 #define _INVENTORY_HPP_
 
 #include "SlotInventory.hpp"
+#include "crafting/Crafting.hpp"
 #include <bits/stdc++.h>
 #include <string>
 #include <fstream>
 using namespace std;
+
+int convertIDtoInt(string ID);
 
 class Inventory {
   protected:
@@ -21,6 +24,8 @@ class Inventory {
     void addItem(Item* item, int quantity = 1);
     void discardItem(string id, int quantity);
     void useTool(string id);
+    void moveItem(string IDsrc, string IDdest);
+    void moveToCrafting(string IDslotInventory, int N, string* IDcraftdest, Crafting table);
 
     // Show Inventory
     void showInventory();
